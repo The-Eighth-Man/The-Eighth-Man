cda27ac59b6f9071575e9b3615174155f8f6668b
Mateo <1u39jfoipasn4ji695263if@example.com>
2022-05-19 19:42:18 -0500

    Prueba final.

--- main.py
+++ main.py
@@ -7,15 +7,11 @@ from pysentimiento import create_analyzer
 
 app = FastAPI()
 
-analyzer = create_analyzer(task="sentiment", lang="es")
-df = pd.read_csv('db.csv')     
-cv = pickle.load(open("count_vectorizer.pickle", "rb"))
-RF = pickle.load(open("RF_nlp.pickle", "rb"))
-
 class Pred(BaseModel):
     review: str
 
 def feeling(review):
+    analyzer = create_analyzer(task="sentiment", lang="es")
     output_sent = analyzer.predict(review).output
     if output_sent == 'POS':
         return 'positivo'
@@ -26,8 +22,11 @@ def feeling(review):
 
 @app.get("/")
 async def read_item(n: int = 10):
+    df = pd.read_csv('db.csv')     
     return df.loc[0:n-1].to_dict('records')
 
 @app.post("/pred/")
 async def create_item(pred: Pred):
+    cv = pickle.load(open("count_vectorizer.pickle", "rb"))
+    RF = pickle.load(open("RF_nlp.pickle", "rb"))
     return {'review': pred.review, 'feeling': feeling(pred.review), 'score': str(RF.predict(cv.transform([pred.review]))[0])}
